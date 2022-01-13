c1c8019f9d875b769d8f2d108a2c03eda09df972
Juliana Arias <gonzalomgomez5@gmail.com>
2022-01-13 18:16:21 -0300

    commit desde-notebook

--- index.js
+++ index.js
@@ -18,12 +18,24 @@ function getServices() {
     })
     .then((data) => {
       const fieldsCollection = data.items.map((items) => {
-        return {
+        const obj = {
           title: items.fields.compusTitle,
           description: items.fields.compusParrafoText,
+          imgId: items.fields.compusImg.sys.id,
+          includes: data.includes.Asset,
         };
+        return obj
+      });
+      fieldsCollection.forEach((i)=>{
+        let idEncontrado = buscarAsset(i.imgId, i.includes);
+        i.img= idEncontrado.fields.file.url;
       });
       return fieldsCollection;
+      function buscarAsset(assetID, includes){
+        return includes.find((item)=>{
+          return item.sys.id == assetID
+        })
+      }
     });
 }
 function servicesIterador() {
@@ -34,9 +46,92 @@ function servicesIterador() {
   });
 }
 
+function bienvenidaContent(params) {
+  const template = document.querySelector("#bienvenida");
+  const bienvenidaDiv = document.querySelector(".titulo1");
+  template.content.querySelector(".titulo__texto").textContent = params.titulo
+  template.content.querySelector(".titulo__sub").textContent = params.subtitulo;
+
+  const clone = document.importNode(template.content, true);
+  bienvenidaDiv.appendChild(clone);
+}
+function getBienvenidaContent(){
+  return fetch("https://cdn.contentful.com/spaces/6i04s7t927m9/environments/master/entries?access_token=e2mGaPLdxAvyOV8VXo5YtwyPVaindzvXp9_LmWw_9mw&content_type=bienvenida")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      const fieldsCollection = data.items.map((item)=>{
+        return{
+           titulo: item.fields.titulo,
+           subtitulo: item.fields.subtitulo
+        }
+      })
+     return fieldsCollection
+    })
+  }
+
+  function bienvenidaIterador() {
+    getBienvenidaContent().then(function (title) {
+      for (const i of title) {
+        bienvenidaContent(i);
+      }
+    });
+  }
+
+  function presentacionContent(params) {
+    const template = document.querySelector("#presentacion");
+    const presentacionDiv = document.querySelector(".presentacion");
+    template.content.querySelector(".contenido__mi-nombre").textContent = params.titulo
+    template.content.querySelector(".sobre-mi").textContent = params.parrafo;
+    template.content.querySelector(".contenido__foto").src = params.img;
+  
+    const clone = document.importNode(template.content, true);
+    presentacionDiv.appendChild(clone);
+  }
+
+  function getPresentacion() {
+    return fetch(
+      "https://cdn.contentful.com/spaces/6i04s7t927m9/environments/master/entries?access_token=e2mGaPLdxAvyOV8VXo5YtwyPVaindzvXp9_LmWw_9mw&content_type=presentacion"
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const fieldsCollection = data.items.map((items) => {
+          const obj = {
+            titulo: items.fields.titulo,
+            parrafo: items.fields.parrafo.content.content,
+            imgId: items.fields.imagen.sys.id,
+            includes: data.includes.Asset,
+          };
+          return obj
+        });
+        fieldsCollection.forEach((i)=>{
+          let idEncontrado = buscarAsset(i.imgId, i.includes);
+          i.img= idEncontrado.fields.file.url;
+        });
+        return fieldsCollection;
+        function buscarAsset(assetID, includes){
+          return includes.find((item)=>{
+            return item.sys.id == assetID
+          })
+        }
+      });
+  }
+  function presentacionIterador() {
+    getPresentacion().then(function (items) {
+      for (const w of items) {
+       presentacionContent(w);
+      }
+    });
+  }
+
 function main() {
   const botonHamburg = document.querySelector(".header__options");
   buttonComponent(botonHamburg);
   servicesIterador();
+  bienvenidaIterador()
+  presentacionIterador()
 }
 main();
